18a396bfd16c23913f1a2cc605d8dde739cad4ca
watermin <tnals08019@gmail.com>
2023-08-20 23:04:50 +0900

    column/row , state

--- BasicsCodelab/app/src/main/java/com/practice/basicscodelab/MainActivity.kt
+++ BasicsCodelab/app/src/main/java/com/practice/basicscodelab/MainActivity.kt
@@ -3,12 +3,22 @@ package com.practice.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,31 +37,94 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun SuminApp(modifier: Modifier = Modifier) {
-    Surface(
-        color = MaterialTheme.colorScheme.background,
-        modifier = modifier
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    Surface(modifier = modifier) {
+        if (shouldShowOnBoarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
     ) {
-        Greeting(name = "Android")
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            onClick = onContinueClicked,
+            modifier = Modifier.padding(vertical = 24.dp)
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    names: List<String> = listOf("World", "Compose"),
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
 private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val extraPadding = if (isExpanded) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { isExpanded = !isExpanded }) {
+                Text(text = if (isExpanded) "Show less" else "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+fun SuminAppPreview() {
     BasicsCodelabTheme {
-        SuminApp()
+        SuminApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview() {
+    BasicsCodelabTheme {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 360)
+@Composable
+fun OnboardingPreview() {
+    BasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
