ea20cbad64a95c35fef7fa6919c524f3bc1b1dbf
watermin <tnals08019@gmail.com>
2023-09-11 02:36:47 +0900

    rememberSaveable로 변경

--- BasicStateCodelab/app/src/main/java/com/codelab/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelab/state/WaterCounter.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +20,7 @@ import com.codelab.state.ui.theme.BasicStateCodelabTheme
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
 //        val count: MutableState<Int> = remember { mutableStateOf(0) }
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
 
         if (count > 0) {
             Text(text = "You've had $count glasses.")
