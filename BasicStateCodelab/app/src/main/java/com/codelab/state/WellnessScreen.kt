bb37f6cafe9666d915efe222b1ea95809faf23ef
watermin <tnals08019@gmail.com>
2023-09-10 21:35:16 +0900

    클릭 이벤트 추가

--- BasicStateCodelab/app/src/main/java/com/codelab/state/WellnessScreen.kt
+++ BasicStateCodelab/app/src/main/java/com/codelab/state/WellnessScreen.kt
@@ -2,8 +2,16 @@ package com.codelab.state
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.state.ui.theme.BasicStateCodelabTheme
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier = modifier)
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 800)
+@Composable
+fun WellnessScreenPreview() {
+    BasicStateCodelabTheme { WellnessScreen() }
 }
