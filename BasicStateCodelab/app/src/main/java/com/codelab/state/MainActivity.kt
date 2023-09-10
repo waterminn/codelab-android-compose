2ba30d21190136285c07e8c363667d1cc15ee92c
watermin <tnals08019@gmail.com>
2023-09-10 21:24:01 +0900

    정적인 state로 구현

---
+++ BasicStateCodelab/app/src/main/java/com/codelab/state/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.codelab.state
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.state.ui.theme.BasicStateCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicStateCodelabTheme {
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    WellnessScreen()
+                }
+            }
+        }
+    }
+}
