634675a12fed500b45c63c597e6e04e7a4e4f6be
Sumin Bae <tnals08019@gmail.com>
2023-10-01 14:24:12 +0900

    Theming codelab (#6)
    * change color
    
    * add dynamic colors
    
    * change typography
    
    * change shapes
    
    * highlight contents

--- ThemingCodelab/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
+++ ThemingCodelab/app/src/main/java/com/example/reply/ui/ReplyListContent.kt
@@ -30,6 +30,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -64,6 +65,8 @@ fun ReplyInboxScreen(
 
         LargeFloatingActionButton(
             onClick = { /*Click Implementation*/ },
+            containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+            contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
             modifier = Modifier
                 .align(Alignment.BottomEnd)
                 .padding(16.dp),
